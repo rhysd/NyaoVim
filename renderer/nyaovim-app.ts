b158412ae6281cd6892126b963d847b6640f6b71
rhysd <lin90162@yahoo.co.jp>
2015-12-17 23:25:22 +0900

    enable to load plugins later and fit window to Vim screen

--- renderer/nyaovim-app.ts
+++ renderer/nyaovim-app.ts
@@ -2,6 +2,7 @@ import {Neovim} from 'neovim-component';
 import {remote, shell} from 'electron';
 import {join} from 'path';
 import {readdirSync} from 'fs';
+import {RPCValue} from 'promised-neovim-client';
 
 class ComponentLoader {
     initially_loaded: boolean;
@@ -12,32 +13,36 @@ class ComponentLoader {
         this.component_paths = [];
     }
 
-    load(path: string) {
+    loadPath(path: string) {
         const link = document.createElement('link') as HTMLLinkElement;
         link.rel = 'import';
         link.href = path;
         document.head.appendChild(link);
     }
 
+    loadPluginDir(dir: string) {
+        try {
+            for (const entry of readdirSync(dir)) {
+                if (entry.endsWith('.html')) {
+                    this.loadPath(entry);
+                    this.component_paths.push(entry);
+                }
+            }
+        } catch (err) {
+            // 'nyaovim-plugin' doesn't exist
+        }
+    }
+
     loadFromRTP(rtp_string: string) {
         const runtimepaths = rtp_string.split(',');
         for (const rtp of runtimepaths) {
-            const nyaovim_dir = join(rtp, 'nyaovim-plugin');
-            try {
-                for (const entry of readdirSync(nyaovim_dir)) {
-                    if (entry.endsWith('.html')) {
-                        this.load(entry);
-                        this.component_paths.push(entry);
-                    }
-                }
-            } catch (err) {
-                // 'nyaovim-plugin' doesn't exist
-            }
+            this.loadPluginDir(join(rtp, 'nyaovim-plugin'));
         }
     }
 }
 
 const component_loader = new ComponentLoader();
+const ThisBrowserWindow = remote.getCurrentWindow();
 
 Polymer({
     is: 'nyaovim-app',
@@ -66,17 +71,43 @@ Polymer({
         });
 
         editor.on('process-attached', () => {
-            editor.getClient()
-                  .eval('&runtimepath')
+            const client = editor.getClient();
+
+            client.eval('&runtimepath')
                   .then((rtp: string) => {
                       component_loader.loadFromRTP(rtp);
                       component_loader.initially_loaded = true;
                   });
+
+            client.on('notification', (method: string, args: RPCValue[]) => {
+                switch (method) {
+                case 'nyaovim:load-path':
+                    component_loader.loadPath(args[0] as string);
+                    break;
+                case 'nyaovim:load-plugin-dir':
+                    component_loader.loadPluginDir(args[0] as string);
+                    break;
+                default:
+                    break;
+                }
+            });
+            client.subscribe('nyaovim:load-path');
+            client.subscribe('nyaovim:load-plugin-dir');
         });
 
         window.addEventListener(
             'resize',
             () => editor.screen.resizeWithPixels(window.innerWidth, window.innerHeight) // XXX
         );
+    },
+
+    attached: function() {
+        // XXX:
+        // Temporary fix.  Resize browser window to fit to content
+        const [win_width, win_height] = ThisBrowserWindow.getContentSize();
+        const canvas = this.editor.screen.canvas;
+        if (win_width !== canvas.width || win_height !== canvas.height) {
+            ThisBrowserWindow.setContentSize(canvas.width, canvas.height);
+        }
     },
 });
