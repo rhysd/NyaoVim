779fb5467db8ade5e35669d716d636cdefc837c1
rhysd <lin90162@yahoo.co.jp>
2016-01-30 19:23:58 +0900

    ensure /usr/local/bin in PATH
    Fixes #25

--- renderer/main.ts
+++ renderer/main.ts
@@ -1,4 +1,14 @@
 import {remote} from 'electron';
+
+if (process.env.PATH.indexOf('/usr/local/bin') === -1) {
+    // Note:
+    // When app is started via clicking NyaoVim.app, $PATH is not set up.
+    // XXX:
+    // This is just a workaround.
+    // If nvim is installed to other directory, we can't know that.
+    process.env.PATH += ':/usr/local/bin';
+}
+
 const nyaovimrc_path: string = remote.getGlobal('nyaovimrc_path');
 if (!nyaovimrc_path) {
     console.error('nyaovimrc is not found in renderer process');
