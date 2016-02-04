51f52a96419417904159a9b054caad86df68eee5
rhysd <lin90162@yahoo.co.jp>
2016-02-04 23:56:18 +0900

    detach editor process

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,17 @@
 #! /usr/bin/env node
 'use strict';
 
+var spawn = require('child_process').spawn;
+var electron = require('electron-prebuilt');
+var join = require('path').join;
+
 var argv = process.argv.slice(2);
-argv.unshift(require('path').join(__dirname, '..'));
+argv.unshift(join(__dirname, '..'));
 if (!process.env['NODE_ENV']) {
     process.env['NODE_ENV'] = 'production';
 }
-require('child_process').spawn(require('electron-prebuilt'), argv, { stdio: 'inherit' });
+spawn(electron, argv, {
+    stdio: 'inherit',
+    detached: true
+}).unref();
 
