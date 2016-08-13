cd2d28fd4866317efa03094440bbc14435bcbe38
rhysd <lin90162@yahoo.co.jp>
2016-08-13 20:45:16 +0900

    fix install script for Travis

--- scripts/travis-before-install.sh
+++ scripts/travis-before-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "TRAVIS_OS_NAME" == "linux" ]]; then
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo add-apt-repository ppa:neovim-ppa/unstable -y
     sudo apt-get -qq update
     sudo apt-get install -y software-properties-common python-software-properties
@@ -8,8 +8,11 @@ if [[ "TRAVIS_OS_NAME" == "linux" ]]; then
     export DISPLAY=:99.0
     sh -e /etc/init.d/xvfb start
     sleep 3
-elif [[ "TRAVIS_OS_NAME" == "osx" ]]; then
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew tap neovim/neovim
     brew update
     brew install --HEAD --with-release neovim/neovim/neovim
+else
+    echo "Unknown platform: ${TRAVIS_OS_NAME}"
+    exit 1
 fi
