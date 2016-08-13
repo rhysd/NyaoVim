26afe2cf5c1a67a77e3de5f596468e92e01c3f94
rhysd <lin90162@yahoo.co.jp>
2016-08-13 19:53:31 +0900

    separate before_install into shell script

---
+++ scripts/travis-before-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ "TRAVIS_OS_NAME" == "linux" ]]; then
+    sudo add-apt-repository ppa:neovim-ppa/unstable -y
+    sudo apt-get -qq update
+    sudo apt-get install -y software-properties-common python-software-properties
+    sudo apt-get install -y neovim
+    export DISPLAY=:99.0
+    sh -e /etc/init.d/xvfb start
+    sleep 3
+elif [[ "TRAVIS_OS_NAME" == "osx" ]]; then
+    brew tap neovim/neovim
+    brew update
+    brew install --HEAD --with-release neovim/neovim/neovim
+fi
