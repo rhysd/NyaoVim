93663c4b03e7bc3432a8ca639389b85803bc5b65
rhysd <lin90162@yahoo.co.jp>
2017-08-11 02:31:42 +0900

    explicitly enable accelerated 2D canvas

--- main/main.ts
+++ main/main.ts
@@ -99,7 +99,7 @@ function startMainWindow() {
         height: 600,
         useContentSize: true,
         webPreferences: {
-            blinkFeatures: 'KeyboardEventKey,Canvas2dFixedRenderingMode',
+            blinkFeatures: 'KeyboardEventKey,Accelerated2dCanvas,Canvas2dFixedRenderingMode',
         },
         icon: nativeImage.createFromPath(join(__dirname, '..', 'resources', 'icon', 'nyaovim-logo.png')),
     } as Electron.BrowserWindowConstructorOptions;
